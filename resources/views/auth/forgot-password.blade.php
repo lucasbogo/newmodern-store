e929fbbf938aaa09d3da70f9fc1fb145e0e16bbd
lucasbogo <lucasbogo93@gmail.com>
2022-06-24 23:19:59 -0300

    password recovery

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,40 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!-- Extender main_master localizado em views/frontend (fragmentação frontend) -->
+@extends('frontend.main_master')
+@section('content')
+    <div class="container">
+        <div class="breadcrumb-inner">
+            <ul class="list-inline list-unstyled">
+                <li><a href="home.html">Home</a></li>
+                <li class="active">Forget Password</li>
+            </ul>
+        </div><!-- /.breadcrumb-inner -->
+    </div><!-- /.container -->
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
+    <div class="container">
+        <div class="sign-in-page">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 sign-in">
+                    <h4 class="">Recover Password</h4>
+                    <p class="">Forgot your Password? No problem!</p>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+                    <!-- RECUPERAÇÃO DE SENHA VIA EMAIL-->
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
 
-        <x-jet-validation-errors class="mb-4" />
+                        <div class="form-group">
+                            <label class="info-title" for="exampleInputpassword1">Email Address <span>*</span></label>
+                            <input type="email" id="email" name="email"
+                                class="form-control unicase-form-control text-input">
+                        </div>
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+                        <button type="submit" class="btn-upper btn btn-primary checkout-page-button">Email Password reset Link</button>
+                    </form>
+                </div>
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+            </div><!-- /.row -->
+        </div><!-- FINAL RECUPERAÇÃO SENHA VIA EMAIL-->
+
+        <!-- INCLUSÃO DAS LOGOMARCAS -->
+        @include('frontend.body.brands')
+    @endsection
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,36 +1,55 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!-- Extender main_master localizado em views/frontend (fragmentação frontend) -->
+@extends('frontend.main_master')
+@section('content')
+    <div class="container">
+        <div class="breadcrumb-inner">
+            <ul class="list-inline list-unstyled">
+                <li><a href="home.html">Home</a></li>
+                <li class="active">Reset Password</li>
+            </ul>
+        </div><!-- /.breadcrumb-inner -->
+    </div><!-- /.container -->
 
-        <x-jet-validation-errors class="mb-4" />
+    <div class="container">
+        <div class="sign-in-page">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 sign-in">
+                    <h4 class="">Recover Password</h4>
+                    <p class="">Forgot your Password? No problem!</p>
 
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+                    <!-- UPDATE DE SENHA-->
+                    <form method="POST" action="{{ route('password.update') }}">
+                        @csrf
 
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                        <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-            </div>
+                        <div class="form-group">
+                            <label class="info-title" for="exampleInputpassword1">Email Address <span>*</span></label>
+                            <input type="email" id="email" name="email"
+                                class="form-control unicase-form-control text-input">
+                        </div>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+                        <div class="form-group">
+                            <label class="info-title" for="exampleInputpassword1">Password <span>*</span></label>
+                            <input type="password" id="password" name="password"
+                                class="form-control unicase-form-control text-input">
+                        </div>
 
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
+                        <div class="form-group">
+                            <label class="info-title" for="exampleInputpassword1">Confirm Password <span>*</span></label>
+                            <input type="password" id="password_confirmation" name="password_confirmation"
+                                class="form-control unicase-form-control text-input">
+                        </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Reset Password') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+                        <button type="submit" class="btn-upper btn btn-primary checkout-page-button">Reset
+                            Password</button>
+                    </form>
+                </div>
+
+
+            </div><!-- /.row -->
+        </div><!-- FINAL RECUPERAÇÃO SENHA VIA EMAIL-->
+
+        <!-- INCLUSÃO DAS LOGOMARCAS -->
+        @include('frontend.body.brands')
+    @endsection
