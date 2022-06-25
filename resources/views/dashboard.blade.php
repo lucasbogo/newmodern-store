fe10699b6064998488df997aa7525481d6ccdf0f
lucasbogo <lucasbogo93@gmail.com>
2022-06-25 00:22:45 -0300

    user profile basic html

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,42 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-           Olá Usuário: {{ Auth::user()->name}}. Só na paz?
-        </h2>
-    </x-slot>
+@extends('frontend.main_master')
+@section('content')
+    <div class="body-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2"><br><br>
+                    <img class="card-img-top" style="border-radius: 50%"
+                        src="{{ !empty($editData->profile_photo_path)
+                            ? url('upload/admin_images/' . $editData->profile_photo_path)
+                            : url('upload/no-image.png') }}"
+                        style=" width: 100px; height: 100px;"><br><br>
+                    <!-- bootstrap class list-group e list-group-flush -->
+                    <ul class="list-group list-group-flush">
+                        <!-- botão primário pequeno -->
+                        <a href="" class="btn btn-primary btn-sm btn-block">Home</a>
+                        <a href="" class="btn btn-primary btn-sm btn-block">Profile Update</a>
+                        <a href="" class="btn btn-primary btn-sm btn-block">Change Password</a>
+                        <a href="" class="btn btn-danger btn-sm btn-block">Logout</a>
 
-    <div class="py-12">
-        Aqui será view HOME
-    </div>
-</x-app-layout>
+                    </ul>
+
+
+                </div><!-- /col -->
+
+                <div class="col-md-2">
+
+
+                </div><!-- /col -->
+
+                <div class="col-md-6">
+                    <div class="card">
+                        <h3 class="text-center"><span class="text-danger">Olá...</span><strong>
+                                {{ Auth::user()->name }}</strong> Bem vindo à NewModern Loja Virtual</h3>
+                    </div>
+
+
+                </div><!-- /col -->
+
+            </div><!-- /row -->
+        </div><!-- /container -->
+    </div><!-- /content -->
+@endsection
