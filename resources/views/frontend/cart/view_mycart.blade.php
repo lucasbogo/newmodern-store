b12bc183341769673bba6884b9b3379c7ac17563
lucasbogo <lucasbogo93@gmail.com>
2022-07-19 00:26:26 -0300

    clean code refactoring

--- resources/views/frontend/cart/view_mycart.blade.php
+++ resources/views/frontend/cart/view_mycart.blade.php
@@ -92,8 +92,96 @@
                             </table>
                         </div>
                     </div>
-                </div><!-- /.row -->
-            </div><!-- /.sigin-in-->
+
+                    <!-- ======================== CAMPO CALCULAR VALOR FRETE (FUTURO) ========================  -->
+                    <div class="col-md-4 col-sm-12 estimate-ship-tax">
+                    </div>
+
+
+
+                    <!-- ======================== ADICIONAR CUPOM/VOUCHERS (FUTURO) ========================  -->
+                    <div class="col-md-4 col-sm-12 estimate-ship-tax">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <span class="estimate-title">
+                                            @if (session()->get('language') == 'portuguese')
+                                                Código de Desconto
+                                            @else
+                                                Discount Code
+                                            @endif
+                                        </span>
+                                        <p>
+                                            @if (session()->get('language') == 'portuguese')
+                                                Insira o código do seu voucher
+                                            @else
+                                                Enter your coupon code
+                                            @endif
+                                        </p>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control unicase-form-control text-input"
+                                                placeholder="Voucher" id="coupon_name">
+                                        </div>
+                                        <div class="clearfix pull-right">
+                                            <button type="submit" class="btn-upper btn btn-primary" onclick="applyCoupn()">
+                                                @if (session()->get('language') == 'portuguese')
+                                                    APLICAR
+                                                    CUPOM
+                                                @else
+                                                    APPLY
+                                                    COUPON
+                                                @endif
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <!-- ======================== CAMPO SUBTOTAL/TOTAL ========================  -->
+                    <div class="col-md-4 col-sm-12 cart-shopping-total">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <div class="cart-sub-total">
+                                            Subtotal<span class="inner-left-md">$600.00</span>
+                                        </div>
+                                        <div class="cart-grand-total">
+                                            Total<span class="inner-left-md">$600.00</span>
+                                        </div>
+                                    </th>
+                                </tr>
+                            </thead><!-- /thead -->
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <div class="cart-checkout-btn pull-right">
+                                            <button type="submit" class="btn btn-primary checkout-btn">
+                                                @if (session()->get('language') == 'portuguese')
+                                                    FAZER O
+                                                    CHECKOUT
+                                                @else
+                                                    PROCEED TO
+                                                    CHEKOUT
+                                                @endif
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
         <br>
 
